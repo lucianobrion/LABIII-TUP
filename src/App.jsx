0feb9e261c7cc35da4e56a75814696ed6b879f11
Luciano Brion <luciano.brion@hotmail.com>
2024-04-05 19:24:23 -0300

    Primer commit - Carga de proyecto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import ReturnMovieTitle from './components/returnMovieTitle/ReturnMovieTitle'
+
+
+function App() {
+  return (
+    <div>
+      <ReturnMovieTitle />
+    </div>
+  )
+};
+
+export default App;
